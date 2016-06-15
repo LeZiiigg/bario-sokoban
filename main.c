4ccc624f4c02e07a0d5f18a01687109813aa0e50
LeZiiigg <darmet.ulysse@gmail.com>
2016-06-15 21:27:14 +0200

    Destroy the window in the main

--- main.c
+++ main.c
@@ -4,13 +4,13 @@
 
 /*	To compile it with gcc use:
 *		gcc main.c -o bario-sokoban -lSDL2
-*/ 
+*/
 
 int main(int argc, char* argv[])
 {
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_Window* window = SDL_CreateWindow("Bario Sokoban", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 560, 420, SDL_WINDOW_SHOWN);
-	
+
 	int quit = 0;
 	while (!quit)
 	{
@@ -26,6 +26,7 @@ int main(int argc, char* argv[])
 		}
 	}
 
+	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return 0;
 }
