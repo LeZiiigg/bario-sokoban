f15f2978956937fa36ebf0e64e39618450ea433c
LeZiiigg <darmet.ulysse@gmail.com>
2016-06-19 14:39:46 +0200

    No longer try to initialize empty elements if elem_zero is NULL

--- array.c
+++ array.c
@@ -4,9 +4,6 @@
 
 #include "array.h"
 
-#define min(a, b) (((a) < (b)) ? (a) : (b))
-#define max(a, b) (((a) > (b)) ? (a) : (b))
-
 void* createArray(size_t array_size, size_t elem_size, void* elem_zero)
 {
 	void* array;
@@ -41,9 +38,9 @@ int resizeArray(void** array, size_t array_size, size_t new_array_size, size_t e
 	if (new_array == NULL)
 		goto exception_new_array_bad_alloc;
 
-	for (i = 0; i < max(new_array_size, array_size); i++)
+	for (i = 0; i < ((new_array_size > array_size && elem_zero != NULL) ? new_array_size : array_size); i++)
 	{
-		if (i < min(new_array_size, array_size))
+		if (i < new_array_size && i < array_size)
 			memcpy((char*)new_array + i * elem_size, (char*)(*array) + i * elem_size, elem_size);
 		else if (i >= array_size && elem_zero != NULL)
 			memcpy((char*)new_array + i * elem_size, elem_zero, elem_size);
