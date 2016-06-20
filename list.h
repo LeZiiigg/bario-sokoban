0b506dc1cd9405ca5525c7f1bef824040456f92f
Ulysse Darmet <darmet.ulysse@gmail.com>
2016-06-20 14:57:00 +0200

    Add the prototypes of the new linked-list module

---
+++ list.h
@@ -0,0 +1,23 @@
+#ifndef __LIST_H__
+#define __LIST_H__
+
+#include <stddef.h>
+
+#define list_member(element, member) ((size_t)(&(element)->member) - (size_t)(element))
+
+void list_insert_first(void* head, void* tail, void* element, size_t next, size_t prev);
+
+void list_insert_last(void* head, void* tail, void* element, size_t next, size_t prev);
+
+void list_insert(void* head, void* tail, position, void* element, size_t next, size_t prev);
+
+void list_remove_first(void* head, void* tail, size_t next, size_t prev, void (*destruct)(void*));
+
+void list_remove_last(void* head, void* tail, size_t next, size_t prev, void (*destruct)(void*));
+
+void list_remove(void* head, void* tail, element, size_t next, size_t prev, void (*destruct)(void*));
+
+void list_clear(void* head, void* tail, size_t next, size_t prev, void (*destruct)(void*));
+
+#endif /* __LIST_H__ */
+
