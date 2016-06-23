f5de7be2ddfd6b260e095b2fc97501763ee95635
LeZiiigg <darmet.ulysse@gmail.com>
2016-06-23 19:40:05 +0200

    Create a renderer and import SDL_image

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
 #include "GamePackage.h"
 
 int main(int argc, char* argv[])
 {
 	SDL_Init(SDL_INIT_VIDEO);
+	IMG_Init(IMG_INIT_PNG);
+
 	SDL_Window* window = SDL_CreateWindow("Bario Sokoban", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 560, 420, SDL_WINDOW_SHOWN);
+	SDL_Renderer* render = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
 	GamePackage* package = loadGamePackage("level.tmx");
 
@@ -28,7 +32,10 @@ int main(int argc, char* argv[])
 
 	destroyGamePackage(package);
 
+
+	SDL_DestroyRenderer(render);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
+	IMG_Quit();
 	return 0;
 }
